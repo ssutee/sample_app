ef8e9e2a1bc86e273c6805303161e17d9edc9a46
Sutee Sudprasert <etipitaka@gmail.com>
2011-04-20 21:42:19 +0700

    Fixed a typo and Finished Exercise 5.5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round") 
+  end
+
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    return base_title if @title.nil?
+    return "#{base_title} | #{@title}"
+  end
 end
